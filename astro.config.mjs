0dc1ca711851bb9b44445afb958bf87c20c9d573
Iida Lehtonen <iida.e.lehtonen@gmail.com>
2026-08-06 07:27:24 +0300

    Improve mobile loading performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://iidalehtonen.com',
+  build: {
+    inlineStylesheets: 'always',
+  },
 });
